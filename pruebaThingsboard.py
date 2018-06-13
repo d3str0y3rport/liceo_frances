477f23b9658ce9ed90030f95b5be123aef27da3f
d3str0y3rport@gmail.com <Ju4nP4bl0!>
2018-06-13 16:46:50 -0400

    a

--- pruebaThingsboard.py
+++ pruebaThingsboard.py
@@ -63,7 +63,8 @@ try:
                     mensaje = b"""{"chip": "1","operation": "getTemp"}""" 
                     mensajeRecibido = 1
                     ser.write(mensaje)
-                #time.sleep(1)
+
+                time.sleep(1)
                 tiempoParaLeer = time.time() + 5
 
             
