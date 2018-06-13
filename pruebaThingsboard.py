ac208d7f4d107fb4739ff96150419cd1f29bf6b7
d3str0y3rport@gmail.com <Ju4nP4bl0!>
2018-06-13 16:03:19 -0400

    A

--- pruebaThingsboard.py
+++ pruebaThingsboard.py
@@ -69,7 +69,7 @@ try:
 
             
         ser.flush()
-        if ser.in_waiting
+        if ser.in_waiting:
 
             recibidoSerial = ser.readline()
             print ("Respuesta recibida: ", recibidoSerial)
