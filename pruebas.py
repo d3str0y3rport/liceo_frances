6fba9c7648934b47ce9f73377c9a4b7c757a974b
d3str0y3rport@gmail.com <Ju4nP4bl0!>
2018-06-13 18:56:59 -0400

    a

--- pruebas.py
+++ pruebas.py
@@ -12,13 +12,14 @@ ser = serial.Serial('/dev/ttyS1', 230400, timeout = 0.1)
 while True:
 	if leido == 1:
 		ser.write(b"""{"chip": "1","operation": "getTemp"}""")
-		print("enviado")
+		print("enviado", time.time())
 		leido = 0
 		ser.flush()
 
 	if ser.in_waiting:
 		mensajerecibido = ser.readline()
 		print ("recibido del serial: ", mensajerecibido)
+		print(time.time())
 		leido = 1
     
 
