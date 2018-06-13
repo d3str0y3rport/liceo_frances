d329522120d22b46c9778a8cfdc5eb399f41b87e
d3str0y3rport@gmail.com <Ju4nP4bl0!>
2018-06-13 18:53:02 -0400

    a

--- pruebas.py
+++ pruebas.py
@@ -4,17 +4,20 @@ import json
 
 mensajeBandera = False
 mensajeRecibido = ""
+leido = 1
 
 ser = serial.Serial('/dev/ttyS1', 230400, timeout = 0.1)
 
 
 while True:
-	ser.write(b"""{"chip": "1","operation": "getTemp"}""")
-	print("enviado")
-	ser.flush()
+	if leido = 1
+		ser.write(b"""{"chip": "1","operation": "getTemp"}""")
+		print("enviado")
+		ser.flush()
 
 	if ser.in_waiting:
 		mensajerecibido = ser.readline()
 		print ("recibido del serial: ", mensajerecibido)
+		leido = 1
     
 
